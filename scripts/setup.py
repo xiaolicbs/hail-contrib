2a837780cdc93df0e8be336c1bb9a61b14d27ffc
tpoterba <tpoterba@broadinstitute.org>
2017-10-04 18:15:41 -0400

    Add scripts

---
+++ scripts/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+def get_tag():
+    f = open('TAG', 'r')
+    tag = f.read()
+    f.close()
+    return tag
+
+
+def get_description():
+    f = open('README', 'r')
+    desc = f.read()
+    f.close()
+    return desc
+
+
+setup(name='hailc',
+      version=get_tag(),
+      description='Lightly reviewed community code snippets for Hail.',
+      long_description=get_description(),
+      url='https://github.com/hail-is/hail-contrib',
+      author='Hail Team',
+      author_email='hail-team@broadinstitute.org',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7'
+      ],
+      keywords='bioinformatics genomics spark hail',
+      packages=['hailc']
+      )
